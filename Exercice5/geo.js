2a35ca13967d698104c68e1189321320927390ab
francoisprost <56918899+francoisprost@users.noreply.github.com>
2020-06-12 05:41:10 +0200

    Fichiers source DevWeb

---
+++ Exercice5/geo.js
@@ -0,0 +1,47 @@
+function getLocation() {
+	if (Modernizr.geolocation) {
+		navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+	}
+}
+
+
+function geoSuccess(positionInfo) {
+	var posESIREM = {latitude: 47.3121519, longitude: 5.0039326 }
+	
+	document.getElementById("longitude").innerHTML = positionInfo.coords.longitude;
+	document.getElementById("latitude").innerHTML = positionInfo.coords.latitude;
+	document.getElementById("precision").innerHTML = positionInfo.coords.accuracy;
+	document.getElementById("altitude").innerHTML = positionInfo.coords.altitude;
+	document.getElementById("precisionAltitude").innerHTML = positionInfo.coords.altitudeAccuracy;
+	document.getElementById("cap").innerHTML = positionInfo.coords.heading;
+	document.getElementById("vitesse").innerHTML = positionInfo.coords.speed;
+	
+	document.getElementById("distance").innerHTML = calculDistance(positionInfo.coords, posESIREM) + " km";
+}
+
+function geoError(positionError) {
+	if (errorInfo.code == 1)
+		alert("L’utilisateur ne souhaite pas partager sa position");
+	else if (errorInfo.code == 2)
+		alert("Impossible de déterminer une position");
+	else if (errorInfo.code == 3)
+		alert("Délai de recherche de position trop long");
+}
+
+function calculDistance(startCoords, destCoords) {
+	var startLatRads = degreesEnRadians(startCoords.latitude);
+	var startLongRads = degreesEnRadians(startCoords.longitude);
+	var destLatRads = degreesEnRadians(destCoords.latitude);
+	var destLongRads = degreesEnRadians(destCoords.longitude);
+	
+	var Radius = 6371; // rayon de la Terre en km
+	var distance = Math.acos(Math.sin(startLatRads) * Math.sin(destLatRads) + Math.cos(startLatRads) * Math.cos(destLatRads) * Math.cos(startLongRads - destLongRads)) * Radius;
+	return distance;
+}
+
+function degreesEnRadians(degrees) {
+	radians = (degrees * Math.PI)/180;
+	return radians;
+}
+
+window.onload = getLocation;
